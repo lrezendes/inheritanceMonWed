ef3ed45f240c76ebf70dbffdd27f0b31c8936c11
laurenrezendes <lrezendes@student.bridgew.edu>
2021-10-06 15:48:14 -0400

    finished class project

--- src/comp/comp152/Doctor.java
+++ src/comp/comp152/Doctor.java
@@ -4,11 +4,18 @@ public class Doctor {
     protected String AlmaMater;
     private double fee;
 
+    public Doctor(String MedSchool, double fee){
+        AlmaMater = MedSchool;
+        this.fee = fee;
+    }
+
     public void treatPatient(Person patient){
-        System.out.println(" Place holder for treat patient");
-        
+        System.out.println("The Doctor says take two of these and call the office in the morning");
+        patient.getTreated();
+
     }
     public double billPatient(Person patient){
+        patient.payBill(fee);
         return fee;
     }
 
@@ -16,5 +23,21 @@ public class Doctor {
         if (isMedicare)
             return fee/2;
         return fee;
+    }
+
+    @Override
+    public String toString(){
+        return "Doctor: \nMed School:" + AlmaMater+"\nFee:"+fee;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Doctor)) {
+            return false;
+        }
+        Doctor otherDoctor = (Doctor) other;
+        var isSame = this.fee == otherDoctor.fee &&this .AlmaMater.equals(otherDoctor.AlmaMater);
+        //ALWAYS use two and signs when using and
+        return isSame;
     }
 }
